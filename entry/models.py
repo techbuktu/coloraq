d94319b76c8284308873534606aec8718f56c962
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-15 00:07:44 -0400

    Created main entry.Entry model

--- entry/models.py
+++ entry/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify 
 
 # Create your models here.
+class Entry(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    color = models.CharField(max_length=25, verbose_name="Preferred Color")
+    link = models.SLugField(max_length=125, blank=True)
+
+    def __str__(self):
+        return "%s %s: %s" % (slef.first_name, self.last_name, self.color)
+
+    def save(self, *args, **kwargs):
+        if not self.link:
+            self.link = slugify(self.first_name + str(self.id))
+        super(Entry, self).save(*args, **kwargs)
+
+
+
