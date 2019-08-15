a5f546b74c968208bf2b7c930e14cff9c6278941
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-15 00:22:55 -0400

    Created the api.serializers.EntrySerializer

---
+++ entry/api/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers 
+from entry.models import Entry 
+
+class EntrySerializer(serializers.HyperlinkedModelSerializer):
+    """
+    An API serializer for the entry.Entry model.
+    """
+    
+    class Meta:
+        model = Entry
+        fields = ('first_name', 'last_name','age','color')
+
+
