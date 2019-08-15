6b5c71b567fb22f25764884eae47c6a326ec332a
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-15 01:17:41 -0400

    Scaffolded EntryService for use in API calls to REST backend

---
+++ entry/static/colorApp/src/app/services/entry.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EntryService {
+
+  constructor() { }
+  
+  getAllEntries(){
+
+  }
+
+  getEntryByUrl(url){
+  
+  }
+
+  newEntry(new_entry){
+  
+  }
+
+  }
