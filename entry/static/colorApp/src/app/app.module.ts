d3c22b3e673201dad899457b2f45363f298c7e63
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-15 01:14:35 -0400

    Scaffolded initial views/components

--- entry/static/colorApp/src/app/app.module.ts
+++ entry/static/colorApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NewEntryComponent } from './components/new-entry/new-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NewEntryComponent
   ],
   imports: [
     BrowserModule,
