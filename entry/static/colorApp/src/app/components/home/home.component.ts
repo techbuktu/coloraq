f46b192c4fe6bf37ef94f60e586566b773d179e3
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-16 00:30:21 -0400

    Imported, injected and use the EntryService to fetch list of Entries from the API for use in HomeComponent

--- entry/static/colorApp/src/app/components/home/home.component.ts
+++ entry/static/colorApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EntryService } from '../../services/entry.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  entries: any;
+  api_error_message: string;
+
+  constructor(private entryApi: EntryService) {
+
+   }
 
   ngOnInit() {
+    this.getEntries();
+  }
+
+  getEntries(){
+    this.entryApi.getAllEntries()
+    .subscribe(
+      response => {
+        console.log(response);
+        this.entries = response;
+        console.log("Entries: ", this.entries);
+      },
+      err => {
+        this.api_error_message = `Sorry, something went wrong. 
+          <p>
+          Please, reload the page and try again.
+          `
+          
+      },
+      () => {
+      }
+    )
   }
 
 }
