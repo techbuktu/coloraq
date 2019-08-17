ba23b928a39195800ad970dbf153f356a54c0da8
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-17 19:15:14 -0400

    Instantiated entryForm in NewEntryComponent

--- entry/static/colorApp/src/app/components/new-entry/new-entry.component.ts
+++ entry/static/colorApp/src/app/components/new-entry/new-entry.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EntryService } from '../../services/entry.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-entry',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewEntryComponent implements OnInit {
 
-  constructor() { }
+  entryForm: FormGroup;
+  newEntry;
+  valid_form: boolean = false;
+
+  constructor(private entryApi: EntryService, private router: Router, private formBuilder: FormBuilder) {
+
+   }
 
   ngOnInit() {
+    this.entryForm = this.formBuilder.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      color: ['', Validators.required]
+    })
+  };
+
+  validateForm(){
+
+    this.postNewEntry()
+  }
+
+  postNewEntry(){
+    //this.router.navigate(['']);
   }
 
 }
