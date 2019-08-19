c9bb97995ffc8d1d4cad0e1c4c7292d792856886
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-19 16:53:46 -0400

    calculating for ageGroup that likes a color most partially implemented

--- entry/static/colorApp/src/app/components/home/home.component.ts
+++ entry/static/colorApp/src/app/components/home/home.component.ts
@@ -52,15 +52,49 @@ export class HomeComponent implements OnInit {
   }
 
   createColorMap(){
-    this.color_map = this.colors.map(color => {
-      return { color: color, ageGroups: []};
+   /* this.color_map = this.colors.map(color => {
+      let ageColorMap = { };
+      //return { "color": color, "ageGroups": []};
+      ageColorMap["color"] = color;
+      ageColorMap["ageGroups"] = [];
+      ageColorMap["faveColor"]= null;
+      return ageColorMap;
    
     })
-    this.createAgeGroupColor();
+    */
+    let colorSet = new Set(this.colors);
+    console.log(colorSet);
+    for(let color of colorSet){
+      //Create an Object to hold the color-to-ageGroup mapping 
+      let ageColorMap = { };
+      ageColorMap["color"] = color;
+      ageColorMap["ageGroups"] = [];
+      ageColorMap["fanGroup"] = 0;
+      console.log(ageColorMap);
+
+      // Populate the list of ageGroups that like a certian color
+      let colorLikes = this.entries.filter(entry => entry.color === ageColorMap["color"]);
+      console.log(colorLikes);
+
+      //Get a list of all the age Groups that like each color ("colorLikes")
+      let ageGroups = [];
+      ageGroups = colorLikes.map(like => like.age);
+      console.log("ageGroups: " + ageGroups);
+      //this.entries.map(entry => entry.color === color)
+      ageColorMap["ageGroups"] = ageGroups;
+
+      this.color_map.push(ageColorMap);
+    }
+    
+    console.log("this.color_map array: " + this.color_map);
+    console.log(this.color_map.length);
+    //this.createAgeGroupColor();
   }
 
-  createAgeGroupColor(){
-
+  createAgeGroupColor(color_name){
+    for (let obj of this.color_map){
+     return "True";
+    }
   }
 
   openForm():void {
