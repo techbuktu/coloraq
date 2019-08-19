113c6d513fde2e571b39ec3de537071c04b70ed3
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-19 19:04:50 -0400

    Cleanups of console.logs() in HomeComponent.ts

--- entry/static/colorApp/src/app/components/home/home.component.ts
+++ entry/static/colorApp/src/app/components/home/home.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { EntryService } from '../../services/entry.service';
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -20,19 +19,15 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.getEntries();
-
   }
 
   getEntries(){
     this.entryApi.getAllEntries()
     .subscribe(
       response => {
-        console.log(response);
         this.entries = response;
-        console.log("Entries: ", this.entries);
       },
       err => {
-        console.log(err);
         this.api_error_message = `Sorry, something went wrong. 
           <p>
           Please, reload the page and try again.
@@ -47,40 +42,28 @@ export class HomeComponent implements OnInit {
 
   getColors(){
     this.colors = this.entries.map(entry => entry.color);
-    console.log("Array of colors:" + this.colors);
+
     this.createColorMap();
   }
 
   createColorMap(){
-   /* this.color_map = this.colors.map(color => {
-      let ageColorMap = { };
-      //return { "color": color, "ageGroups": []};
-      ageColorMap["color"] = color;
-      ageColorMap["ageGroups"] = [];
-      ageColorMap["faveColor"]= null;
-      return ageColorMap;
-   
-    })
-    */
     let colorSet = new Set(this.colors);
-    console.log(colorSet);
+    
     for(let color of colorSet){
       //Create an Object to hold the color-to-ageGroup mapping 
       let ageColorMap = { };
       ageColorMap["color"] = color;
       ageColorMap["ageGroups"] = [];
-      ageColorMap["favGroup"] = 0;
+      ageColorMap["favGroup"] = null;
       console.log(ageColorMap);
 
       // Populate the list of ageGroups that like a certian color
       let colorLikes = this.entries.filter(entry => entry.color === ageColorMap["color"]);
-      console.log(colorLikes);
 
       //Get a list of all the age Groups that like each color ("colorLikes")
       let ageGroups = [];
       ageGroups = colorLikes.map(like => like.age);
-      console.log("ageGroups: " + ageGroups);
-      //this.entries.map(entry => entry.color === color)
+
       ageColorMap["ageGroups"] = ageGroups;
 
       // Get most popular color within each ageGroup
@@ -89,9 +72,6 @@ export class HomeComponent implements OnInit {
       this.color_map.push(ageColorMap);
     }
     
-    console.log("FINAL this.color_map array: " + this.color_map);
-    console.log(this.color_map.length);
-    //this.createAgeGroupColor();
   }
 
   getFanAgeGroup(ageColorMap){
@@ -112,17 +92,10 @@ export class HomeComponent implements OnInit {
         compare = counts[group];
         favGroup = group;
         ageColorMap.favGroup = favGroup;
-        console.log("This Group: ageGroups=" + ageColorMap.ageGroups + "and faveGroup is:" + ageColorMap.favGroup);
       }
 
     }
-  }
-
-  createAgeGroupColor(color_name){
-    for (let obj of this.color_map){
-     return "True";
-    }
-  }
+  };
 
   openForm():void {
     this.router.navigate(['/new']);
